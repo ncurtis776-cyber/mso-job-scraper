1466352100d082adf88fa311de6615bdb860cf58
ncurtis776-cyber <ncurtis776@my.cleary.edu>
2026-05-28 19:16:23 -0400

    Update clean_scraper.py

--- clean_scraper.py
+++ clean_scraper.py
@@ -1,24 +1,23 @@
 import requests
-import requests
 from bs4 import BeautifulSoup
 from datetime import datetime
 import gspread
 from google.oauth2.service_account import Credentials
-
+import os
+import json
 
 scopes = [
     "https://www.googleapis.com/auth/spreadsheets",
     "https://www.googleapis.com/auth/drive"
 ]
 
-import os
-import json
-
+# ✅ LOAD CREDS FROM GITHUB SECRET (NOT FILE)
 creds_dict = json.loads(os.environ["GOOGLE_CREDS"])
 creds = Credentials.from_service_account_info(creds_dict, scopes=scopes)
 client = gspread.authorize(creds)
 
 sheet = client.open_by_url("https://docs.google.com/spreadsheets/d/1FkEqxI_ZhpdaUD1AxyV_oGTW3mHXo-sBb4FKGSZ8hD0").worksheet("Jobs")
+
 companies = [
     {"name": "Curaleaf", "url": "https://boards.greenhouse.io/embed/job_board?for=curaleaf"},
     {"name": "Cresco Labs", "url": "https://boards.greenhouse.io/embed/job_board?for=crescolabs"},
